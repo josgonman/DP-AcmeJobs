3b7281b24c2749303ffdd3fc8650e22ea8af7bd6
Jose Manuel <josgonman@alum.us.es>
2019-10-01 12:34:35 +0200

    WorkSpace 2.19

---
+++ Workspace-2.19/Scrapbook/drop-database.sql
@@ -0,0 +1,13 @@
+-- drop-database.sql
+--
+-- Copyright (c) 2019 Rafael Corchuelo.
+--
+-- In keeping with the traditional purpose of furthering education and research, it is
+-- the policy of the copyright owner to permit non-commercial use and redistribution of
+-- this software. It has been tested carefully, but it is not guaranteed for any particular
+-- purposes. The copyright owner does not offer any warranties or representations, nor do
+-- they accept any liabilities with respect to them.
+
+-- Change "Starter-Project" according to the name of your project.
+
+drop database if exists `Starter-Project`;
