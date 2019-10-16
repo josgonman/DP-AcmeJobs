7400deb4d99fc50f3bc14820da0e0f9c461d68c5
Jose Manuel <josgonman@alum.us.es>
2019-10-16 12:07:23 +0200

    Creado Shout Y ListShout y funcina

---
+++ Starter-Project/src/main/java/acme/entities/shouts/Shout.java
@@ -0,0 +1,37 @@
+
+package acme.entities.shouts;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
+
+import acme.framework.entities.DomainEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Shout extends DomainEntity {
+
+	// Identificador para ser serielizable
+
+	private static final long	serialVersionUID	= 1L;
+
+	//Atributos
+
+	@NotBlank
+	private String				author;
+
+	@NotBlank
+	private String				text;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	private Date				moment;
+
+}
